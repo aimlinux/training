ecc703209174be4b0ae520f15fd267c7b0198c00
aimlinux <114636143+aimlinux@users.noreply.github.com>
2023-05-06 12:55:26 +0900

    Summary?

---
+++ C_train/Pointers_05.06/Use_array/test_1/test_1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(void) {
+
+    //宣言と同時に初期化
+    int int_array_1[5] = {0, 1, 2, 3, 4};
+    printf("%d\n", int_array_1[0]);
+
+
+    //全ての要素をゼロで初期化
+    int int_array_2[] = {};
+    for(int i = 0; i < 5; i++) {
+        if(i < 4) {
+            printf("%d", int_array_2[i]);
+        } else if(i == 4) {
+            printf("%d\n", int_array_2[i]);
+        }
+    }
+
+
+    //途中まで初期化
+    int int_array_3[] = {0, 1, 2};
+    for(int i = 0; i < 5; i++) {
+        if(i < 4) {
+            printf("%d,", int_array_3[i]);
+        } else {
+            printf("%d\n", int_array_3[i]);
+        }
+    }
+
+
+    //一つの要素だけゼロ以外を指定
+    int int_array_4[5] = {[2] = 2};
+    for(int i = 0; i < 5; i++) {
+        if(i < 4) {
+            printf("%d,", int_array_4[i]);
+        } else {
+            printf("%d\n", int_array_4[i]);
+        }
+    }
+
+
+    //総素数の記述を省略
+    char char_array_1[] = {'H', 'E', 'L', 'L', 'O'};
+    printf("%s\n", char_array_1); 
+
+    return 0; 
+}
